c6780b2690c2636836c6c7a3cf174ac06e3f0f62
Alejandro Barrios <barrilocho@gmail.com>
2022-04-09 18:52:06 -0500

    feat(component): added code for JobExperience compnent

--- src/presentation/components/JobExperience/JobExperience.test.tsx
+++ src/presentation/components/JobExperience/JobExperience.test.tsx
@@ -1,35 +1,37 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import JobExperience from './JobExperience';
 
-describe('Test DescriptionField component', () => {
-  test('Should render component DescriptionField', () => {
-    const component = render(<JobExperience />);
-    const element = component.getByText('JobExperience');
-    expect(element).toBeInTheDocument();
+describe('Test JobExperience component', () => {
+  const companyName = 'Aleate';
+  const jobTitle = 'Test job description';
+  const workTime = 'Test work time';
+  const companyLogo = '/img/aleate_logo.png';
+  const place = 'Test place';
+  let component: RenderResult;
+  beforeEach(() => {
+    component = render(
+      <JobExperience
+        workTime={workTime}
+        companyName={companyName}
+        companyLogo={companyLogo}
+        jobTitle={jobTitle}
+      />
+    );
   });
   test('Should render company name', () => {
-    const companyName = 'Aleate';
-    const component = render(<JobExperience companyName={companyName}/>);'
     const element = component.getByText(companyName);
     expect(element).toBeInTheDocument();
   });
   test('Should render job description', () => {
-    const jobDescription = 'Test job description';
-    const component = render(<JobExperience jobDescription={jobDescription}/>);
-    const element = component.getByText(jobDescription);
+    const element = component.getByText(jobTitle);
     expect(element).toBeInTheDocument();
   });
   test('Should render work time', () => {
-    const workTime = 'Test work time';
-    const component = render(<JobExperience workTime={workTime}/>);
     const element = component.getByText(workTime);
     expect(element).toBeInTheDocument();
   });
   test('Should render company logo', () => {
-    const companyLogo = '/img/aleate_logo.png';
-    const component = render(<JobExperience companyLogo={companyLogo}/>); 
     const element = component.getByAltText('Aleate logo');
     expect(element).toBeInTheDocument();
   });
-
 });
--- src/presentation/components/JobExperience/JobExperience.tsx
+++ src/presentation/components/JobExperience/JobExperience.tsx
@@ -1,8 +1,41 @@
-const JobExperience = () => {
+import Image from 'next/image';
+import { FC } from 'react';
+
+interface Props {
+  companyName: string;
+  jobTitle: string;
+  workTime: string;
+  companyLogo: string;
+  place?: string;
+}
+
+const JobExperience: FC<Props> = ({
+  companyLogo,
+  companyName,
+  workTime,
+  jobTitle,
+  place,
+}) => {
   return (
-    <div>
-      <h1>JobExperience</h1>
-    </div>
+    <>
+      <div className="flex my-2">
+        <div className="mr-4 pt-1">
+          <Image
+            src={companyLogo}
+            width={60}
+            height={60}
+            alt={`${companyName} logo`}
+            className="rounded-full"
+          />
+        </div>
+        <div>
+          <h4 className="font-bold mt-0 text-gray-600">{jobTitle}</h4>
+          <h4 className="font-medium text-gray-500">{companyName}</h4>
+          <h5 className="text-sm italic text-gray-500">{workTime}</h5>
+          <h5 className="text-sm italic text-gray-500">{place}</h5>
+        </div>
+      </div>
+    </>
   );
 };
 
