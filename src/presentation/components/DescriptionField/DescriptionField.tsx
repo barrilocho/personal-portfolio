8cbc09ae4841de36d2e8d912788f0efeee529234
Alejandro Barrios <barrilocho@gmail.com>
2022-04-08 23:11:59 -0500

    feat(component): description field pass test complete

--- src/presentation/components/DescriptionField/DescriptionField.tsx
+++ src/presentation/components/DescriptionField/DescriptionField.tsx
@@ -1,7 +1,28 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
-interface Props {}
+interface Props {
+  text?: string;
+  icon?: () => ReactNode;
+  link?: string;
+}
 
-export const DescriptionField: FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+export const DescriptionField: FC<Props> = ({ children, text, icon, link }) => {
+  return (
+    <div>
+      {children ? (
+        children
+      ) : (
+        <div>
+          <>{icon ? icon() : null}</>
+          {link ? (
+            <a target="_blank" rel="noreferrer" href={link}>
+              {text ? text : link}
+            </a>
+          ) : (
+            text
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
