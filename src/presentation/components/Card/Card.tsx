7e274fe83e044d389285fc64b160c1e967fec34d
Alejandro Barrios <barrilocho@gmail.com>
2022-04-16 10:25:09 -0500

    feat(resume): resume portfolio mobile

--- src/presentation/components/Card/Card.tsx
+++ src/presentation/components/Card/Card.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Card: FC<Props> = ({ title, children }) => {
   return (
-    <section className="relative flex flex-col p-6 pt-4 bg-white justify-center shadow-sm max-w-sm sm:box-border sm:basis-auto sm:h-auto  sm:rounded-md md:m-6">
+    <section className="relative flex flex-col p-6 pt-4 bg-white justify-center w-full shadow-sm max-w-sm sm:box-border sm:basis-auto sm:h-auto  sm:rounded-md md:m-6">
       {title && (
         <header className="font-medium my-2 pb-2 border-b-2 border-gray-200 border-dashed">
           {title}
