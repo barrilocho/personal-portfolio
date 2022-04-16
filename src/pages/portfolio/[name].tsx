41effc67da84d01d1e0d4567c4cbdadd26377602
Alejandro Barrios <barrilocho@gmail.com>
2022-04-16 10:15:16 -0500

    feat(ecosan): add ecosan portfolio and image

--- src/pages/portfolio/[name].tsx
+++ src/pages/portfolio/[name].tsx
@@ -1,4 +1,5 @@
 import type { GetStaticProps, NextPage } from 'next';
+import Image from 'next/image';
 
 interface Props {
   name: string;
@@ -9,36 +10,47 @@ interface Props {
 
 const ProjectPage: NextPage<Props> = ({ name = 'ecosan' }) => {
   return (
-    <div className={`bg-${name} bg-fixed bg-center bg-no-repeat`}>
+    <div className={`bg-${name} bg-cover bg-fixed bg-center bg-no-repeat`}>
       <div className="p-5">
         <h1 className="text-6xl font-bold text-center text-white">
-          Ecosan app
+          Ecosan App
         </h1>
-        <p className="pt-16 text-white text-xl w- text-justify">
-          Una aplicacion hecha con react native, para el director de operaciones
-          y los operadores de la empresa ecosan, la cual consiste en medir el
-          rendimiento de
+        <div className="mt-10 flex flex-row">
+          <Image
+            className="absolute t-10 l-1"
+            src={`/img/portfolio/${name}/${name}1.png`}
+            height={'800px'}
+            width={'1000px'}
+            alt={`${name} portfolio 1`}
+          />
+          <Image
+            className="absolute t-10 l-1"
+            src={`/img/portfolio/${name}/${name}2.png`}
+            height={'800px'}
+            width={'1000px'}
+            alt={`${name} portfolio 2`}
+          />
+        </div>
+        <p className="pt-16 mb-12 text-white text-xl w- text-justify">
+          Una aplicacion hecha con react native, permite conocer en tiempo real
+          el status de una empresa, para llevar un mejor control interno y
+          control de inventario. Ayuda a analizar y contrastar einformacion para
+          mejorar el desempeño de la operación
         </p>
-        <p className="pt-16 text-white text-xl w- text-justify">
-          Una aplicacion hecha con react native, para el director de operaciones
-          y los operadores de la empresa ecosan, la cual consiste en medir el
-          rendimiento de
-        </p>
-        <p className="pt-16 text-white text-xl w- text-justify">
-          Una aplicacion hecha con react native, para el director de operaciones
-          y los operadores de la empresa ecosan, la cual consiste en medir el
-          rendimiento de
-        </p>
-        <p className="pt-16 text-white text-xl w- text-justify">
-          Una aplicacion hecha con react native, para el director de operaciones
-          y los operadores de la empresa ecosan, la cual consiste en medir el
-          rendimiento de
-        </p>
-        <p className="pt-16 text-white text-xl w- text-justify">
-          Una aplicacion hecha con react native, para el director de operaciones
-          y los operadores de la empresa ecosan, la cual consiste en medir el
-          rendimiento de
-        </p>
+        <Image
+          className="mb-12"
+          src={`/img/portfolio/${name}/${name}3.png`}
+          height={'1000px'}
+          width={'500px'}
+          alt={`${name} portfolio 1`}
+        />
+        <Image
+          className="w-full"
+          src={`/img/portfolio/${name}/${name}4.png`}
+          height={'100%'}
+          width="100%"
+          alt={`${name} portfolio 1`}
+        />
       </div>
     </div>
   );
