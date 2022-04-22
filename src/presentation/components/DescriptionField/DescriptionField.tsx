3a8980fffc2fd8ce43a661f88047721f749d034c
Alejandro Barrios <barrilocho@gmail.com>
2022-04-21 23:41:32 -0500

    feat(dark): add darkmode to description field

--- src/presentation/components/DescriptionField/DescriptionField.tsx
+++ src/presentation/components/DescriptionField/DescriptionField.tsx
@@ -13,13 +13,17 @@ export const DescriptionField: FC<Props> = ({ children, text, icon, link }) => {
         children
       ) : (
         <div className="text-blue-500 flex items-center font-medium ">
-          <div className="mr-2 text-gray-500">{icon ? icon : null}</div>
+          <div className="mr-2 text-gray-500 dark:text-gray-100 ">
+            {icon ? icon : null}
+          </div>
           {link ? (
             <a target="_blank" rel="noreferrer" href={link}>
               {text ? text : link}
             </a>
           ) : (
-            <h4 className="font-medium text-gray-500">{text}</h4>
+            <h4 className="font-medium text-gray-500 dark:text-gray-100 ">
+              {text}
+            </h4>
           )}
         </div>
       )}
