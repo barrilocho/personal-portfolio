3ba3fcb02870868d2d91e8c2da4facdadf036703
Alejandro Barrios <barrilocho@gmail.com>
2022-04-23 22:48:09 -0500

    feat(portfolio): custom scroll and less animation

--- src/presentation/components/Skill/Skill.tsx
+++ src/presentation/components/Skill/Skill.tsx
@@ -9,7 +9,7 @@ const Skill = ({ name, src, rounded }: Props) => {
   return (
     <div className="group flex flex-wrap cursor-default">
       <div
-        className={`flex w-fit mr-2 mt-2 py-2 px-4 bg-blue-50 dark:bg-slate-600 duration-[2s] ease-out group-hover:bg-blue-300 ${
+        className={`flex w-fit mr-2 mt-2 py-2 px-4 bg-blue-50 dark:bg-slate-600 duration-[1s] ease-out group-hover:bg-blue-300 ${
           rounded ? 'rounded-3xl' : ''
         }`}
       >
