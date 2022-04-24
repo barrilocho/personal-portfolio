fff719b80ecfa365b3482e4311d58d851fb8719f
Alejandro Barrios <barrilocho@gmail.com>
2022-04-23 22:47:04 -0500

    Feat(animations): add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,32 @@ module.exports = {
         ecosan: "url('/img/portfolio/ecosan/bg_ecosan.svg')",
         arca: "url('/img/portfolio/arca/bg_arca.svg')",
       },
+      keyframes: {
+        fadeInUp: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(-100%)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
+        fadeInDown: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(110%)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 1.2s ease-in-out 1',
+        fadeInDown: 'fadeInDown 1s ease-in-out 1',
+      },
     },
   },
   plugins: [],
